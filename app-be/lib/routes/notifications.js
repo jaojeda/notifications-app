b2264ca5ab26d7978f4a5df905a3e2dbf99e1157
Jose Ojeda <j.a.ojeda@live.com>
2022-07-18 20:49:02 -0700

    wrote delete route

--- app-be/lib/routes/notifications.js
+++ app-be/lib/routes/notifications.js
@@ -3,11 +3,25 @@ const fs = require("fs/promises");
 
 const fileName = "/Users/jose/practice/notifications-app/app-be/test.json";
 
-module.exports = Router().get("/", async (req, res, next) => {
-  try {
-    const data = await fs.readFile(fileName, "utf8");
-    res.send(data);
-  } catch (err) {
-    return next(err);
-  }
-});
+module.exports = Router()
+  .get("/", async (req, res, next) => {
+    try {
+      const data = await fs.readFile(fileName, "utf8");
+      res.send(data);
+    } catch (err) {
+      return next(err);
+    }
+  })
+  .delete("/:id", async (req, res, next) => {
+    try {
+      const data = await fs.readFile(fileName, "utf8");
+      const formattedData = JSON.parse(data);
+      const newData = JSON.stringify(
+        formattedData.filter((obj) => obj.id !== Number(req.params.id))
+      );
+      await fs.writeFile(fileName, newData);
+      res.send(newData);
+    } catch (err) {
+      return next(err);
+    }
+  });
