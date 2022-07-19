211cb999b4fdfd99b2b366a83323468cbf78cd02
Jose Ojeda <j.a.ojeda@live.com>
2022-07-18 21:16:37 -0700

    removed create-react-app CSS and components

--- app-fe/src/App.js
+++ app-fe/src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useEffect } from "react";
 
 function App() {
@@ -12,24 +10,8 @@ function App() {
 
     fetchNotifications().catch(console.error);
   }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
